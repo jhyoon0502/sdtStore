e2cf202c8139f5016ef0245ad716c5443502ff28
Yoon Jihun <jhyoon@sdt.inc>
2022-06-28 10:28:35 +0900

    카테고리 작업

--- js/index.js
+++ js/index.js
@@ -4,10 +4,25 @@ window.addEventListener("DOMContentLoaded", function () {
     e.preventDefault();
   });
 
+  const selectAll = $("#selectAll");
+  selectAll.on("click", function () {
+    if (selectAll.prop("checked")) {
+      $("input[name=menu]").prop("checked", true);
+    } else {
+      $("input[name=menu]").prop("checked", false);
+    }
+  });
+
   const cateA = $(".cate-a");
   cateA.on("click", function () {
-    cateA.toggleClass("on");
-    cateA.not($(this)).removeClass("on");
+    $(this).toggleClass("on");
+    $(this).not($(this)).removeClass("on");
+
+    if ($(".cate-a").hasClass("on")) {
+      $(this).next().css("display", "block");
+    } else {
+      $(this).next().css("display", "none");
+    }
   });
 
   $(".go-top").click(function () {
